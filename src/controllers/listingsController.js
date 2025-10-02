159c238daade7afcfb968b38b5ef3ecb351fbcc3
Logan McNatt <logan.m.mcnatt@gmail.com>
2025-10-02 13:33:44 -0600

    Post request to return potential valid listing locations

--- src/controllers/listingsController.js
+++ src/controllers/listingsController.js
@@ -16,6 +16,56 @@ const getAllListings = (req, res) => {
   }
 };
 
+const searchListings = (req, res) => {
+  try {
+    const vehicles = req.body;
+
+    const totalSpaceNeeded = vehicles.reduce((acc, vehicle) => {
+      return acc + (vehicle.length * 10 * vehicle.quantity);
+    }, 0);
+
+    const listingLocations = Object.values(listings.reduce((acc, listing) => {
+      const locationId = listing.location_id;
+
+      if (!acc[locationId]) {
+        acc[locationId] = {
+          location_id: locationId,
+          listing_ids: [],
+          total_square_footage: 0,
+          total_price_in_cents: 0
+        };
+      }
+      
+      acc[locationId].listing_ids.push(listing.id);
+      acc[locationId].total_square_footage += (listing.length * listing.width);
+      acc[locationId].total_price_in_cents += listing.price_in_cents;
+      
+      return acc;
+    }, {}));
+
+    const validListingLocations = listingLocations.filter(listingLocation => {
+      return listingLocation.total_square_footage >= totalSpaceNeeded;
+    });
+
+    res.json({
+      success: true,
+      
+      data: {
+        totalSpaceNeeded,
+        validListingLocations
+      },
+      count: validListingLocations.length
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error searching listings',
+      error: error.message
+    });
+  }
+};
+
 module.exports = {
-  getAllListings
+  getAllListings,
+  searchListings
 };
--- src/routes/listingsRoutes.js
+++ src/routes/listingsRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllListings,
+  searchListings,
 } = require('../controllers/listingsController');
 
 router.get('/', getAllListings);
+router.post('/search', searchListings);
 
 module.exports = router;
